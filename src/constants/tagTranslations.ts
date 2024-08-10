e9e49801c0f6d9fa9e92cabc798e7c1ab4247460
SohMitian <pix4soh@gmail.com>
2024-08-10 21:40:56 +0900

    翻訳リスト更新

--- src/constants/tagTranslations.ts
+++ src/constants/tagTranslations.ts
@@ -1,23 +1,46 @@
 const tagTranslations: { [key: string]: string } = {
+  // core
   "core:creationDate": "データ作成日",
   "core:terminationDate": "データ削除日",
-  "bldg:Building[@gml:id]": "GML ID",
-  "uro:buildingID": "建物ID",
+
+  // gml
   "gml:posList": "建物の座標(経度、緯度)",
-  "bldg:measuredHeight": "建物の高さ(m)",
-  "bldg:storeysAboveGround": "地上階数",
-  "bldg:storeysBelowGround": "地下階数",
-  "xAL:CountryName": "国名",
-  "xAL:LocalityName": "建物の住所(市町村、区、郡、市)",
   "gml:name": "建物の名称",
   "gml:description": "建物の説明",
 
-  "uro:vacancy": "空家状態",
-  "uro:buildingCoverageRate": "建蔽率",
-  "uro:floorAreaRate": "容積率",
-  "uro:specifiedBuildingCoverageRate": "指定建蔽率",
-  "uro:specifiedFloorAreaRate": "指定容積率",
-  "uro:surveyYear": "調査年",
+  // bldg
+  "bldg:Building@gml:id": "GML ID",
+  "bldg:class": "建物の分類",
+  "bldg:usage": "建物の用途",
+  "bldg:measuredHeight": "建物の高さ(m)",
+  "bldg:storeysAboveGround": "地上階数",
+  "bldg:storeysBelowGround": "地下階数",
+
+  // xAL
+  "xAL:CountryName": "国名",
+  "xAL:LocalityName": "建物の住所(市町村、区、郡、市)",
+
+  // uro
+  "uro:buildingID": "建物ID",
+  "uro:prefecture": "都道府県コード",
+  "uro:city": "市区町村コード",
+  "uro:buildingRoofEdgeArea": "建物の屋根の面積(m2)",
+  "uro:fireproofStructureType": "耐火構造タイプ",
+  "uro:urbanPlanType": "都市計画タイプ",
+  "uro:areaClassificationType": "地域分類タイプ",
+  "uro:districtsAndZonesType": "地区・区域タイプ",
+  "uro:landUseType": "土地利用タイプ",
+  "uro:detailedUsage": "詳細用途",
+  "uro:description": "説明",
+  "uro:rank": "ランク",
+  "uro:depth": "深さ(m)",
+  "uro:adminType": "行政区分",
+  "uro:scale": "規模",
+  "uro:geometrySrcDesc": "幾何源説明",
+  "uro:thematicSrcDesc": "主題源説明",
+  "uro:appearanceSrcDesc": "外観源説明",
+  "uro:lod1HeightType": "LOD1の高さ区分",
+  "uro:lod2HeightType": "LOD2の高さ区分",
   "uro:lodType": "LODの区分",
   "uro:realEstateIDOfBuilding": "建築物全体の不動産ID",
   "uro:numberOfBuildingUnit":  "建築物の区分所有の数量",
@@ -26,12 +49,21 @@ const tagTranslations: { [key: string]: string } = {
   "uro:buildingArea": "建物の面積(m2)",
   "uro:buildingAreaCoefficient": "延べ面積換算係数",
   "uro:realEstateIDOfLand": "土地の不動産ID",
+  "uro:buildingCoverageRate": "建蔽率",
+  "uro:floorAreaRate": "容積率",
+  "uro:specifiedBuildingCoverageRate": "指定建蔽率",
+  "uro:specifiedFloorAreaRate": "指定容積率",
+  "uro:surveyYear": "調査年",
+  "uro:vacancy": "空家状態",
+  "uro:buildingDataQualityAttribute": "建物のデータ品質属性",
+  "uro:key": "キー",
+  "uro:codeValue": "コード値",
 };
 
 
 export const ALLOWED_KEYS = [
   "gen:stringAttribute[@name]",
-  "bldg:Building[@gml:id]",
+  "bldg:Building@gml:id",
   "uro:buildingID",
   "gml:posList",
   "bldg:measuredHeight",
